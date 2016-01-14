87b829b361e703774221779a60129fb5579b2d25
rainlclaire <qin.yue@husky.neu.edu>
2016-01-13 20:15:28 -0500

    change the message

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var app = express();
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 app.get('/hello', function(req, res){
-    res.send('hello world1');
+    res.send('hello world1111');
 });
 app.listen(port, ipaddress);
