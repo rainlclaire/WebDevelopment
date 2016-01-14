579995df5a6a2619a1abc5d3b178b066a99ac2e9
rainlclaire <qin.yue@husky.neu.edu>
2016-01-13 20:32:02 -0500

    error changed

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP ||;
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 app.get('/hello', function(req, res){
     res.send('hello world22');
