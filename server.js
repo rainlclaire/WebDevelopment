746be94600a429767a1af46d0df806b8a349fa3c
rainlclaire <qin.yue@husky.neu.edu>
2016-01-14 16:35:36 -0500

    change the ignore file and page text

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
-app.get('/hello', function(req, res){
-    res.send('hello world22');
-
-});
+app.get('/hello', sayhello);
+function sayhello(req, res) {
+    res.send('hello world1');
+}
+//app.get('/hello', function(req, res){
+//    res.send('hello world22');
+//
+//});
 app.listen(port, ipaddress);
