0cc880dbc683041e0756ee4f62515805c2eb19ce
rainlclaire <qin.yue@husky.neu.edu>
2016-01-19 23:43:35 -0500

    update server

--- server.js
+++ server.js
@@ -1,23 +1,8 @@
 var express = require('express');
 var app = express();
-//var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-//var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
-//app.use(express.static(__dirname + '/public'));
-//function sayhello(req,res) {
-//    res.send('hello world1');
-//}
-//
-//function getAllUsers(req, res) {
-//    var user = [{"usersname": "claire", "firstname": "qin", "lastname":"yueling"},
-//        {"usersname": "claire", "firstname": "qin", "lastname":"yueling"}
-//
-//    ]
-//    res.json(user)
-//    //res.send("object literal" );
-//}
-//app.get('/hello', function(req, res){
-//    res.send('hello world22');
-//
-//});
-app.use(express.static(__dirname + '/public'));
-app.listen(3000);
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+app.get('/hello', function(req, res){
+    res.send('hello world');
+});
+app.listen(port, ipaddress);
