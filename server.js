6f352dbffa5f4a3ff9de73a0201ca0969372e095
rainlclaire <qin.yue@husky.neu.edu>
2016-01-19 19:44:46 -0500

    Add the exerises file and part of Assignment1

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 var express = require('express');
 var app = express();
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
-app.get('/hello', sayhello);
-function sayhello(req, res) {
-    res.send('hello world1');
-}
+//var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+//var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+//app.use(express.static(__dirname + '/public'));
+//function sayhello(req,res) {
+//    res.send('hello world1');
+//}
+//
+//function getAllUsers(req, res) {
+//    var user = [{"usersname": "claire", "firstname": "qin", "lastname":"yueling"},
+//        {"usersname": "claire", "firstname": "qin", "lastname":"yueling"}
+//
+//    ]
+//    res.json(user)
+//    //res.send("object literal" );
+//}
 //app.get('/hello', function(req, res){
 //    res.send('hello world22');
 //
 //});
-app.listen(port, ipaddress);
+app.use(express.static(__dirname + '/public'));
+app.listen(3000);
