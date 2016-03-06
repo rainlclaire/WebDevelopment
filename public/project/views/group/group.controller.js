bed6500aea0f43bfcf29e13167be845278c4726c
rainlclaire <qin.yue@husky.neu.edu>
2016-03-06 17:46:52 -0500

    fix bug

--- public/project/views/group/group.controller.js
+++ public/project/views/group/group.controller.js
@@ -8,12 +8,10 @@
 
         var user = $rootScope.currentUser;
 
-            GroupService.findAllGroups(function(allGroups) {
-                $scope.groups= allGroups;
+        GroupService.findAllGroups(function (allGroups) {
+            $scope.groups = allGroups;
 
-            });
-
-
+        });
 
 
         //function initMap() {
@@ -43,15 +41,13 @@
         //}
 
 
-
-        var clickGroup  = $rootScope.clickGroup;
+        var clickGroup = $rootScope.clickGroup;
         //function for form
         $scope.addGroup = addGroup;
         $scope.updateGroup = updateGroup;
         $scope.deleteGroup = deleteGroup;
         $scope.selectGroup = selectGroup;
         //$scope.search = search;
-
 
 
         //function search(group) {
@@ -66,77 +62,70 @@
         //    });
 
 
-
-            //GoogleMapService.searchMapByAddress(group)
-            //    .then(function(response) {
-            //
-            //        console.log(response);
-            //        $scope.results = response.data.results;
-            //        $scope.address = response.data.results[0].formatted_address;
-            //        $scope.url = "https://www.google.com/maps/embed/v1/place?key=AIzaSyBFek2cKN2fA6seFcgfsEDyhE3CONb0ynM&q="+$scope.adress;
-            //
-            //        console.log($scope.results[0].geometry.location.lat);
-
-
-                    //function initMap() {
-                    //    var map = new google.maps.Map(document.getElementById('map'), {
-                    //        zoom: 8,
-                    //        center: {lat: -34.397, lng: 150.644}
-                    //    });
-                    //    var geocoder = new google.maps.Geocoder();
-                    //
-                    //    document.getElementById('submit').addEventListener('click', function() {
-                    //        geocodeAddress(geocoder, map);
-                    //    });
-                    //}
-                    //
-                    //function geocodeAddress(geocoder, resultsMap) {
-                    //    var address = document.getElementById('address').value;
-                    //    geocoder.geocode({'address': address}, function(results, status) {
-                    //        if (status === google.maps.GeocoderStatus.OK) {
-                    //            resultsMap.setCenter(results[0].geometry.location);
-                    //            var marker = new google.maps.Marker({
-                    //                map: resultsMap,
-                    //                position: results[0].geometry.location
-                    //            });
-                    //        } else {
-                    //            alert('Geocode was not successful for the following reason: ' + status);
-                    //        }
-                    //    });
-                    //}
-                    //
-                    //var myLatLng= {
-                    //    lat: $scope.result.value
-                    //}
-                //});
+        //GoogleMapService.searchMapByAddress(group)
+        //    .then(function(response) {
+        //
+        //        console.log(response);
+        //        $scope.results = response.data.results;
+        //        $scope.address = response.data.results[0].formatted_address;
+        //        $scope.url = "https://www.google.com/maps/embed/v1/place?key=AIzaSyBFek2cKN2fA6seFcgfsEDyhE3CONb0ynM&q="+$scope.adress;
+        //
+        //        console.log($scope.results[0].geometry.location.lat);
 
 
+        //function initMap() {
+        //    var map = new google.maps.Map(document.getElementById('map'), {
+        //        zoom: 8,
+        //        center: {lat: -34.397, lng: 150.644}
+        //    });
+        //    var geocoder = new google.maps.Geocoder();
+        //
+        //    document.getElementById('submit').addEventListener('click', function() {
+        //        geocodeAddress(geocoder, map);
+        //    });
+        //}
+        //
+        //function geocodeAddress(geocoder, resultsMap) {
+        //    var address = document.getElementById('address').value;
+        //    geocoder.geocode({'address': address}, function(results, status) {
+        //        if (status === google.maps.GeocoderStatus.OK) {
+        //            resultsMap.setCenter(results[0].geometry.location);
+        //            var marker = new google.maps.Marker({
+        //                map: resultsMap,
+        //                position: results[0].geometry.location
+        //            });
+        //        } else {
+        //            alert('Geocode was not successful for the following reason: ' + status);
+        //        }
+        //    });
+        //}
+        //
+        //var myLatLng= {
+        //    lat: $scope.result.value
+        //}
+        //});
 
-
-
-
-        }
 
         //add the form to currentForms
         function addGroup(group) {
-            var newGroup= {
+            var newGroup = {
                 title: group.title,
                 _id: group._id,
                 ownerName: group.ownerName,
-                description:group.description,
-                listofEvents:group.listofEvents,
-                address:group.address
+                description: group.description,
+                listofEvents: group.listofEvents,
+                address: group.address
             };
 
             //inti the title with empty
-            $scope.clickGroup.title="";
-            $scope.clickGroup.ownerName="";
-            $scope.clickGroup.address="";
-            $scope.clickGroup.description="";
-            $scope.clickGroup.listofEvents="";
+            $scope.clickGroup.title = "";
+            $scope.clickGroup.ownerName = "";
+            $scope.clickGroup.address = "";
+            $scope.clickGroup.description = "";
+            $scope.clickGroup.listofEvents = "";
 
-            GroupService.createGroup(newGroup, function(createdGroups) {
-                GroupService.findAllGroups(function(allGroups) {
+            GroupService.createGroup(newGroup, function (createdGroups) {
+                GroupService.findAllGroups(function (allGroups) {
                     $scope.groups = allGroups;
 
                 });
@@ -162,7 +151,7 @@
         function deleteGroup(index) {
             var deletedId = $scope.groups[index]._id;
             console.log(deletedId);
-            GroupService.deleteGroupById(deletedId, function(allOtherGroups) {
+            GroupService.deleteGroupById(deletedId, function (allOtherGroups) {
                 $scope.groups = allOtherGroups;
             });
         }
@@ -183,6 +172,7 @@
             console.log($scope.clickGroup.ownerName);
 
         }
+    }
 
 
 })();
