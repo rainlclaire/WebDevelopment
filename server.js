2ee0780cd3868990d157b6ddbe5f8b9eb43a1f7b
rainlclaire <qin.yue@husky.neu.edu>
2016-03-14 21:12:34 -0400

    update server

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 
+
+
+
+var express = require('express');
+var app = express();
 var bodyParser = require("body-parser");
 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-
-var express = require('express');
-var app = express();
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
