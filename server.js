de130e10a2c12cb694b3295544ca5a416a888fbf
rainlclaire <qin.yue@husky.neu.edu>
2016-03-29 16:40:57 -0400

    rever my severjs

--- server.js
+++ server.js
@@ -1,33 +1,67 @@
+////
+////
+////
+////
+////var express = require('express');
+////var app = express();
+////var bodyParser = require("body-parser");
+////
+////require("./assignment/server/app.js")(app);
+////app.use(bodyParser.json());
+////app.use(bodyParser.urlencoded({ extended: true }));
+////app.use(express.static(__dirname + '/public'));
+////var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+////var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+////
+////
+////if (typeof ipaddress === "undefined") {
+////    //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
+////    //  allows us to run/test the app locally.
+////    console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
+////    ipaddress = "127.0.0.1";
+////};
+////app.listen(port, ipaddress);
 //
 //
-//
-//
+////  OpenShift sample Node application
 //var express = require('express');
-//var app = express();
 //var bodyParser = require("body-parser");
+//var mongoose = require("mongoose");
+//var multer  = require("multer");
 //
-//require("./assignment/server/app.js")(app);
+//var app = express();
 //app.use(bodyParser.json());
 //app.use(bodyParser.urlencoded({ extended: true }));
 //app.use(express.static(__dirname + '/public'));
-//var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-//var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+//app.get("/", function(req, res) {
+//    res.sendfile('index.html', {root: __dirname });
+//});
 //
+//
+//
+//var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+//var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+//var myDataBase = "/cs4550assignment";
 //
 //if (typeof ipaddress === "undefined") {
 //    //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
 //    //  allows us to run/test the app locally.
 //    console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
 //    ipaddress = "127.0.0.1";
-//};
+//
+//}
+//mongoose.connect("mongodb://"+ipaddress+myDataBase);
+//
+//var db = mongoose.connection;
+//
+//require("./public/assignment/server/app.js")(app, db,mongoose);
+//require("./public/project/server/app.js")(app,db,mongoose);
+//
+//
 //app.listen(port, ipaddress);
 
-
-//  OpenShift sample Node application
 var express = require('express');
 var bodyParser = require("body-parser");
-var mongoose = require("mongoose");
-var multer  = require("multer");
 
 var app = express();
 app.use(bodyParser.json());
@@ -37,25 +71,17 @@ app.get("/", function(req, res) {
     res.sendfile('index.html', {root: __dirname });
 });
 
-
+require("./public/assignment/server/app.js")(app);
+require("./public/project/server/app.js")(app);
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
-var myDataBase = "/cs4550assignment";
 
 if (typeof ipaddress === "undefined") {
     //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
     //  allows us to run/test the app locally.
     console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
     ipaddress = "127.0.0.1";
-
-}
-mongoose.connect("mongodb://"+ipaddress+myDataBase);
-
-var db = mongoose.connection;
-
-require("./public/assignment/server/app.js")(app, db,mongoose);
-require("./public/project/server/app.js")(app,db,mongoose);
-
+};
 
 app.listen(port, ipaddress);
