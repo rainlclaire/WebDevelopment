0138f67a93918265a27a7b4db5505edf4c674cfb
rainlclaire <qin.yue@husky.neu.edu>
2016-03-30 15:11:07 -0400

    add the db for openshift now and debug again

--- server.js
+++ server.js
@@ -24,12 +24,13 @@
 
 
 //  OpenShift sample Node application
-var express = require('express');
+var express = require("express");
+var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var multer  = require("multer");
 
-var app = express();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
@@ -42,6 +43,7 @@ app.get("/", function(req, res) {
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 var myDataBase = "/cs4550assignment";
+var connectionString = "mongodb://"+"127.0.0.1/cs4550assignment";
 
 if (typeof ipaddress === "undefined") {
     //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
@@ -50,9 +52,18 @@ if (typeof ipaddress === "undefined") {
     ipaddress = "127.0.0.1";
 
 }
-mongoose.connect("mongodb://"+"localhost/cs4550assignment");
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+        process.env.OPENSHIFT_APP_NAME;
+}
 
-var db = mongoose.connection;
+
+
+
+var db = mongoose.connect(connectionString);
 
 require("./public/assignment/server/app.js")(app, db,mongoose);
 require("./public/project/server/app.js")(app,db,mongoose);
